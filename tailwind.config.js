ddf4d13b641a0061e4334966f9ebd113e91200ba
Rick-te-Molder <rick.te.molder@gmail.com>
2025-10-28 21:31:21 +0100

    feat: dark theme + Tailwind components on index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
+  darkMode: "class",
   theme: { extend: {} },
   plugins: [],
 };
